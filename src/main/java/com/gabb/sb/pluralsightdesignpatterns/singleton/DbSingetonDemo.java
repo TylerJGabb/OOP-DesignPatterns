43cbd03473b5b852a51ebe30b8f2d8d483df6440
Tyler Gabb <tylerjgabb@gmail.com>
2020-03-02 22:16:16 -0700

    turned DbSingetonDemo into worst case concurrent access demo

--- src/main/java/com/gabb/sb/pluralsightdesignpatterns/singleton/DbSingetonDemo.java
+++ src/main/java/com/gabb/sb/pluralsightdesignpatterns/singleton/DbSingetonDemo.java
@@ -2,11 +2,34 @@ package com.gabb.sb.pluralsightdesignpatterns.singleton;
 
 public class DbSingetonDemo {
 
-    public static void main(String[] args) {
-        var instance1 = DbSingleton.getInstance();
-        var instance2 = DbSingleton.getInstance();
-        System.out.println(instance1);
-        System.out.println(instance2);
-        System.out.println(instance1 == instance2);
+    private static volatile boolean start = false;
+    private static DbSingleton i1 = null;
+    private static DbSingleton i2 = null;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        var t1 = new Thread(() -> {
+            while(!start) Thread.onSpinWait();
+            i1 = DbSingleton.getInstance();
+        });
+        t1.setName("A");
+        var t2 = new Thread(() -> {
+            while(!start) Thread.onSpinWait();
+            i2 = DbSingleton.getInstance();
+        });
+        t2.setName("B");
+
+        t1.start();
+        t2.start();
+
+        //set the start flag, instantly triggering both threads to begin competing for the reference
+        start = true;
+
+        t1.join();
+        t2.join();
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i1 == i2);
     }
 }
--- src/main/java/com/gabb/sb/pluralsightdesignpatterns/singleton/DbSingleton.java
+++ src/main/java/com/gabb/sb/pluralsightdesignpatterns/singleton/DbSingleton.java
@@ -13,9 +13,14 @@ public class DbSingleton {
     }
 
     public static DbSingleton getInstance() {
+        var name = Thread.currentThread().getName();
+        System.out.println(name + " 1");
         if(instance == null){ //check once to prevent performance hit introduced by synchronization
+            System.out.println(name + " 2");
             synchronized (DbSingleton.class) { // synchronize on the DbSingleton class
+                System.out.println(name + " 3");
                 if(instance == null) {              //if the instance is still null then we need to create it
+                    System.out.println(name + " 4");
                     instance = new DbSingleton();   // otherwise, another thread already got here
                 }
             }
