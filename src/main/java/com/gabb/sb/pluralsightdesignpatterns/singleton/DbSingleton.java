0c90e5909d4570a7927825447c7d17bb02ff2109
Tyler Gabb <tylerjgabb@gmail.com>
2020-03-02 21:57:17 -0700

    added a few notes to DbSingleton

--- src/main/java/com/gabb/sb/pluralsightdesignpatterns/singleton/DbSingleton.java
+++ src/main/java/com/gabb/sb/pluralsightdesignpatterns/singleton/DbSingleton.java
@@ -2,6 +2,7 @@ package com.gabb.sb.pluralsightdesignpatterns.singleton;
 
 public class DbSingleton {
 
+    //volatile to ensure instance stays the same across threads
     private static volatile DbSingleton instance;
 
     private DbSingleton() {
@@ -12,10 +13,10 @@ public class DbSingleton {
     }
 
     public static DbSingleton getInstance() {
-        if(instance == null){
-            synchronized (DbSingleton.class) {
-                if(instance == null) {
-                    instance = new DbSingleton();
+        if(instance == null){ //check once to prevent performance hit introduced by synchronization
+            synchronized (DbSingleton.class) { // synchronize on the DbSingleton class
+                if(instance == null) {              //if the instance is still null then we need to create it
+                    instance = new DbSingleton();   // otherwise, another thread already got here
                 }
             }
         }
