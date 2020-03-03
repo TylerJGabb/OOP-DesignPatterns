d6bc5e78ca1ae23c1cd8bef48f2215b20552d697
Tyler Gabb <tylerjgabb@gmail.com>
2020-03-02 21:20:19 -0700

    init

---
+++ src/main/java/com/gabb/sb/pluralsightdesignpatterns/PluralsightDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.gabb.sb.pluralsightdesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluralsightDesignPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluralsightDesignPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabb/sb/pluralsightdesignpatterns/PluralsightDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabb.sb.pluralsightdesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluralsightDesignPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
